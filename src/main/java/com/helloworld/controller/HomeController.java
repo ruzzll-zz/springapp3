f113cefeff6d666c6b1044aaf633306c20fc4479
ruzzll <ruzzll7@gmail.com>
2014-04-01 12:52:29 -0500

    testing: lap

--- src/main/java/com/helloworld/controller/HomeController.java
+++ src/main/java/com/helloworld/controller/HomeController.java
@@ -21,6 +21,7 @@ public class HomeController {
 	
 	/**
 	 * Simply selects the home view to render by returning its name.
+	 * example :D
 	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
