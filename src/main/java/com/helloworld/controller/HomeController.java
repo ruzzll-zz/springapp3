777c013089f22edd40256bdce6598b8cd4bb0384
ruzzll <ruzzll@hotmail.com>
2014-04-02 18:58:48 -0500

    experiment branch

--- src/main/java/com/helloworld/controller/HomeController.java
+++ src/main/java/com/helloworld/controller/HomeController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * Handles requests for the application home page.
- * example :D
+ * this is a branch
  */
 @Controller
 public class HomeController {
